b408d085cfe3f11b5e8f20a204fe03fe29dbb942
Rodrigo Ruotolo <roderickruotolo@gmail.com>
2023-01-12 17:42:11 -0300

    Update address_libs.sh
    Add implements to some functions

--- address_libs.sh
+++ address_libs.sh
@@ -6,22 +6,49 @@ export BOOK
 
 confirm()
 {
-	echo "Confirma"
+	echo -en "$@"
+	read ans
+	ans =`echo $ans | tr '[a-z]' '[A-Z]'`
+	if [ "$ans" == "Y" ]; then 
+		return 0
+	else 
+		return 1
+	fi
 }
 
 num_lines()
 {
-	echo "Num lines"
+	grep -i "$@" $BOOK|wc -l| awk '{ print $1 }'
 }
 
 find_lines()
 {
-	echo "Find Lines"
+	res=-1
+	if [ ! -z "$1" ]; then 
+		grep -i "$@" $BOOK
+		res=$?
+	fi
+	return $res
 }
 
 list_items()
 {
-	echo "List Itens"
+	if [ "$#" -eq "0" ]; then 
+		echo -en "Busca por: (retorna para toda lista) "
+		read search 
+		if [ -z "$search" ]; then
+			search="."
+		fi
+		echo
+	else 
+		search="$@"
+	fi
+	find_lines "${search}" | while read i
+	do
+		echo "$i" | tr ':' '\t'
+	done
+	echo -en "Contatos encontrados: "
+	num_lines
 }
 
 add_item()
